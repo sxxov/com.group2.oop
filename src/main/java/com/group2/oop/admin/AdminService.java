2d668a196434f0749d69ce4f7341ba3bfa89ae53
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-11-02 19:47:08 +0800

    Refactor admin image form management
    * Pick & action approach instead of other way around

--- src/main/java/com/group2/oop/admin/AdminService.java
+++ src/main/java/com/group2/oop/admin/AdminService.java
@@ -4,10 +4,8 @@ import com.group2.oop.account.AccountManager;
 import com.group2.oop.account.AccountService;
 import com.group2.oop.account.UserRepository;
 import com.group2.oop.account.UserRole;
-import com.group2.oop.console.Console;
 import com.group2.oop.dependency.D;
-import com.group2.oop.form.ImageForm;
-import com.group2.oop.form.ImageFormManager;
+import com.group2.oop.form.AdminImageFormService;
 import com.group2.oop.home.HomeService;
 import com.group2.oop.service.Engine;
 import com.group2.oop.service.Service;
@@ -19,7 +17,6 @@ public class AdminService implements Service {
 	public final UserRepository userRepository = D.get(UserRepository.class);
 	public final AccountManager account = D.get(AccountManager.class);
 	private final Scanner scanner = D.get(Scanner.class);
-	private final ImageFormManager imageFormManager = new ImageFormManager();
 
 	private final Service next;
 
@@ -29,25 +26,6 @@ public class AdminService implements Service {
 
 	public AdminService(Service next) {
 		this.next = next;
-	}
-
-	private void printImageForms(ImageForm[] imageForms) {
-		System.out.println("---");
-		for (int i = 0; i < imageForms.length; i++) {
-			var image = imageForms[i];
-			var submitter = userRepository.get(image.submitter());
-
-			System.out.println(
-				(i + 1) +
-				". " +
-				(submitter == null ? "<anonymous>" : submitter.email()) +
-				"\t : " +
-				image.src() +
-				"\t - " +
-				image.status().get()
-			);
-		}
-		System.out.println("---");
 	}
 
 	@Override
@@ -65,13 +43,8 @@ public class AdminService implements Service {
 			System.out.println(
 				"------------------- Admin Menu -------------------"
 			);
-			System.out.println("1. Approve an image");
-			System.out.println("2. Reject an image");
-			System.out.println("3. Show all pending images");
-			System.out.println("4. Show all approved images");
-			System.out.println("5. Show all rejected images");
-			System.out.println("6. Show all images");
-			System.out.println("7. Manage vouchers");
+			System.out.println("1. Manage submitted images");
+			System.out.println("2. Manage vouchers");
 			System.out.println("");
 			System.out.println("0. Exit");
 			System.out.print("> ");
@@ -85,148 +58,10 @@ public class AdminService implements Service {
 
 			switch (choice) {
 				case 1:
-					System.out.println("[Approve an image]\n");
-					var approvedCount = 0;
-					for (;;) {
-						var approvableImages = imageFormManager
-							.allEveryonePending()
-							.toArray(new ImageForm[0]);
-
-						if (approvableImages.length == 0) {
-							System.out.println("No more images to approve.");
-							break;
-						}
-
-						printImageForms(approvableImages);
-						System.out.println("");
-						System.out.println("0. Exit");
-						System.out.println(
-							"Enter the number of the image to approve: "
-						);
-						System.out.print("> ");
-						int i;
-
-						try {
-							i = Integer.parseInt(scanner.nextLine());
-						} catch (NumberFormatException e) {
-							System.out.println("Invalid number.");
-							continue;
-						}
-
-						if (i == 0) break;
-
-						if (i < 1 || i > approvableImages.length) {
-							System.out.println(
-								"Invalid number. It must be between 1-" +
-								approvableImages.length +
-								"."
-							);
-							continue;
-						}
-
-						imageFormManager.approve(
-							approvableImages[i - 1].submitter(),
-							approvableImages[i - 1].src()
-						);
-						System.out.println("Image approved.");
-						System.out.println("");
-						++approvedCount;
-					}
-					System.out.println(approvedCount + " images approved.");
-					Console.waitForEnter();
-					engine.reload();
+					System.out.print("[Manage submitted images]\n");
+					engine.swap(new AdminImageFormService(this));
 					return;
 				case 2:
-					System.out.println("[Reject an image]\n");
-					var rejectCount = 0;
-					for (;;) {
-						var rejectableImages = imageFormManager
-							.allEveryonePending()
-							.toArray(new ImageForm[0]);
-
-						if (rejectableImages.length == 0) {
-							System.out.println("No more images to reject.");
-							break;
-						}
-
-						printImageForms(rejectableImages);
-						System.out.println("");
-						System.out.println("0. Exit");
-						System.out.println(
-							"Enter the number of the image to reject: "
-						);
-						System.out.print("> ");
-						int i;
-
-						try {
-							i = Integer.parseInt(scanner.nextLine());
-						} catch (NumberFormatException e) {
-							System.out.println("Invalid number.");
-							continue;
-						}
-
-						if (i == 0) break;
-
-						if (i < 1 || i > rejectableImages.length) {
-							System.out.println(
-								"Invalid number. It must be between 1-" +
-								rejectableImages.length +
-								"."
-							);
-							continue;
-						}
-
-						imageFormManager.reject(
-							rejectableImages[i - 1].submitter(),
-							rejectableImages[i - 1].src()
-						);
-						System.out.println("Image rejected.");
-						System.out.println("");
-						++rejectCount;
-					}
-					System.out.println(rejectCount + " images rejected.");
-					Console.waitForEnter();
-					engine.reload();
-					return;
-				case 3:
-					System.out.println("[Show all pending images]\n");
-					printImageForms(
-						imageFormManager
-							.allEveryonePending()
-							.toArray(new ImageForm[0])
-					);
-					Console.waitForEnter();
-					engine.reload();
-					return;
-				case 4:
-					System.out.println("[Show all approved images]\n");
-					printImageForms(
-						imageFormManager
-							.allEveryoneApproved()
-							.toArray(new ImageForm[0])
-					);
-					Console.waitForEnter();
-					engine.reload();
-					return;
-				case 5:
-					System.out.println("[Show all rejected images]\n");
-					printImageForms(
-						imageFormManager
-							.allEveryoneRejected()
-							.toArray(new ImageForm[0])
-					);
-					Console.waitForEnter();
-					engine.reload();
-					return;
-				case 6:
-					System.out.print("[Show all images]\n");
-					printImageForms(
-						imageFormManager.allEveryone().toArray(new ImageForm[0])
-					);
-					Console.waitForEnter();
-					engine.reload();
-					return;
-				case 7:
 					System.out.print("[Manage vouchers]\n");
 					engine.swap(new AdminVoucherService(this));
 					return;
---
+++ src/main/java/com/group2/oop/form/AdminImageFormService.java
@@ -0,0 +1,363 @@
+package com.group2.oop.form;
+
+import com.group2.oop.account.AccountManager;
+import com.group2.oop.account.AccountService;
+import com.group2.oop.account.UserRepository;
+import com.group2.oop.account.UserRole;
+import com.group2.oop.carbon_credits.CarbonCreditAccountManager;
+import com.group2.oop.console.Console;
+import com.group2.oop.dependency.D;
+import com.group2.oop.home.HomeService;
+import com.group2.oop.service.Engine;
+import com.group2.oop.service.Service;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.function.Supplier;
+
+public class AdminImageFormService implements Service {
+
+	private final UserRepository userRepository = D.get(UserRepository.class);
+	private final AccountManager account = D.get(AccountManager.class);
+	private final Scanner scanner = D.get(Scanner.class);
+	private final ImageFormManager imageFormManager = new ImageFormManager();
+	private final CarbonCreditAccountManager carbonCreditAccountManager = D.get(
+		CarbonCreditAccountManager.class
+	);
+
+	private final Service next;
+
+	public AdminImageFormService() {
+		this.next = new HomeService();
+	}
+
+	public AdminImageFormService(Service next) {
+		this.next = next;
+	}
+
+	private void printImageForms(ImageForm[] imageForms) {
+		System.out.println("---");
+		for (int i = 0; i < imageForms.length; i++) {
+			var image = imageForms[i];
+			var submitter = userRepository.get(image.submitter());
+
+			System.out.println(
+				(i + 1) +
+				". " +
+				(submitter == null ? "<anonymous>" : submitter.email()) +
+				"\t : " +
+				image.src() +
+				"\t - " +
+				image.status().get()
+			);
+		}
+		System.out.println("---");
+	}
+
+	private void manage(Supplier<ImageForm[]> getPendingImages) {
+		var approvedCount = 0;
+		var rejectedCount = 0;
+
+		main:for (;;) {
+			var pendingImages = getPendingImages.get();
+
+			ImageForm pickImage;
+			pick:for (;;) {
+				System.out.println("Select an image to manage:");
+				printImageForms(pendingImages);
+				System.out.println("");
+				System.out.println("0. Exit");
+
+				int pendingIndex;
+				pickIndex:for (;;) {
+					try {
+						System.out.print("> ");
+						pendingIndex = Integer.parseInt(scanner.nextLine());
+					} catch (NumberFormatException e) {
+						System.out.println("Invalid image number.");
+						continue pickIndex;
+					}
+
+					if (pendingIndex == 0) break main;
+
+					if (
+						pendingIndex < 1 || pendingIndex > pendingImages.length
+					) {
+						System.out.println(
+							"Invalid number. It must be between 1-" +
+							pendingImages.length +
+							"."
+						);
+						continue pickIndex;
+					}
+
+					break;
+				}
+
+				pickImage = pendingImages[pendingIndex - 1];
+				System.out.println(
+					"[" + pendingIndex + ". " + pickImage.src() + "]\n"
+				);
+				break pick;
+			}
+
+			var submitter = userRepository.get(pickImage.submitter());
+			manage:for (;;) {
+				System.out.println("Image URL: " + pickImage.src());
+				System.out.println(
+					"Image submitter: " +
+					(submitter == null ? "<anonymous>" : submitter.email())
+				);
+				System.out.println("Image status: " + pickImage.status().get());
+				System.out.println("");
+				System.out.println("1. View image");
+				System.out.println("2. Approve");
+				System.out.println("3. Reject");
+				System.out.println("");
+				System.out.println("0. Pick another image");
+
+				int manageChoice;
+				manageChoice:for (;;) {
+					System.out.print("> ");
+					try {
+						manageChoice = Integer.parseInt(scanner.nextLine());
+					} catch (NumberFormatException e) {
+						System.out.println("Invalid choice.");
+						continue manageChoice;
+					}
+
+					switch (manageChoice) {
+						case 1:
+							System.out.println("[View image]\n");
+							try {
+								Desktop
+									.getDesktop()
+									.open(new File(pickImage.src()));
+							} catch (IOException e) {
+								System.out.println(
+									"Failed to open image file."
+								);
+								e.printStackTrace();
+							} catch (IllegalArgumentException e) {
+								System.out.println(
+									"Image file does not exist."
+								);
+							}
+							continue manage;
+						case 2:
+							System.out.println("[Approve]\n");
+							if (
+								pickImage.status().get() ==
+								ImageFormStatus.APPROVED
+							) {
+								System.out.println(
+									"Image is already approved."
+								);
+								continue manage;
+							}
+
+							System.out.println(
+								"Enter the number of carbon credits to award: "
+							);
+
+							double approveReward;
+							approveReward:for (;;) {
+								try {
+									System.out.print("> ");
+									approveReward =
+										Double.parseDouble(scanner.nextLine());
+								} catch (NumberFormatException e) {
+									System.out.println(
+										"Invalid reward amount."
+									);
+									continue approveReward;
+								}
+
+								break;
+							}
+
+							pickImage.approve();
+							System.out.println("Image approved.");
+							if (submitter != null) {
+								var carbonCreditAccount = carbonCreditAccountManager.account(
+									submitter.uuid()
+								);
+
+								if (carbonCreditAccount == null) {
+									System.out.println(
+										"Skipped reward because user does not have a carbon credit account."
+									);
+								} else {
+									carbonCreditAccount.deposit(approveReward);
+									System.out.println(
+										"User was awarded " +
+										approveReward +
+										" carbon credits."
+									);
+								}
+							}
+
+							System.out.println("");
+							++approvedCount;
+							continue main;
+						case 3:
+							System.out.println("[Reject]\n");
+							if (
+								pickImage.status().get() ==
+								ImageFormStatus.REJECTED
+							) {
+								System.out.println(
+									"Image is already rejected."
+								);
+								continue manage;
+							}
+							pickImage.reject();
+							System.out.println("Image rejected.");
+							System.out.println("");
+							++rejectedCount;
+							continue main;
+						case 0:
+							break main;
+						default:
+							System.out.println("Invalid choice.");
+							continue manageChoice;
+					}
+				}
+			}
+		}
+
+		System.out.println(
+			"Approved " +
+			approvedCount +
+			" images, rejected " +
+			rejectedCount +
+			" images."
+		);
+		Console.waitForEnter();
+	}
+
+	@Override
+	public void init(Engine engine) {
+		if (
+			account.current().isEmpty() ||
+			account.current().get().role() != UserRole.ADMIN
+		) {
+			engine.swap(new AccountService());
+			return;
+		}
+
+		main:for (;;) {
+			System.out.println(
+				"------------------- Manage images -------------------"
+			);
+			System.out.println("1. Approve or reject an image");
+			System.out.println("2. Manage previous approvals or rejections");
+			System.out.println("");
+			System.out.println("0. Exit");
+
+			int choice;
+			mainChoice:for (;;) {
+				System.out.print("> ");
+				try {
+					choice = Integer.parseInt(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Invalid choice.");
+					continue mainChoice;
+				}
+
+				switch (choice) {
+					case 1:
+						System.out.println("[Approve or reject an image]\n");
+
+						manage(() ->
+							imageFormManager
+								.allEveryonePending()
+								.toArray(new ImageForm[0])
+						);
+
+						continue main;
+					case 2:
+						System.out.println(
+							"[Manage previous approvals or rejections]\n"
+						);
+						history:for (;;) {
+							System.out.println("1. Manage approved images");
+							System.out.println("2. Manage rejected images");
+							System.out.println(
+								"3. Manage approved or rejected images"
+							);
+							System.out.println("");
+							System.out.println("0. Exit");
+
+							int historyFilterChoice;
+							Supplier<ImageForm[]> getHistoryImages;
+							historyFilterChoice:for (;;) {
+								System.out.print("> ");
+								try {
+									historyFilterChoice =
+										Integer.parseInt(scanner.nextLine());
+								} catch (NumberFormatException e) {
+									System.out.println("Invalid choice.");
+									continue historyFilterChoice;
+								}
+
+								switch (historyFilterChoice) {
+									case 1:
+										System.out.println(
+											"[Manage approved images]\n"
+										);
+										getHistoryImages =
+											() ->
+												imageFormManager
+													.allEveryoneApproved()
+													.toArray(new ImageForm[0]);
+										break historyFilterChoice;
+									case 2:
+										System.out.println(
+											"[Manage rejected images]\n"
+										);
+										getHistoryImages =
+											() ->
+												imageFormManager
+													.allEveryoneRejected()
+													.toArray(new ImageForm[0]);
+										break historyFilterChoice;
+									case 3:
+										System.out.println(
+											"[Manage approved or rejected images]\n"
+										);
+										getHistoryImages =
+											() ->
+												imageFormManager
+													.allEveryone()
+													.toArray(new ImageForm[0]);
+										break historyFilterChoice;
+									case 0:
+										System.out.println("[Exit]\n");
+										break main;
+									default:
+										System.out.println("Invalid choice.");
+										continue historyFilterChoice;
+								}
+							}
+
+							manage(getHistoryImages);
+							continue history;
+						}
+					case 0:
+						System.out.println("[Exit]\n");
+						break main;
+					default:
+						System.out.println("Invalid choice.");
+						continue mainChoice;
+				}
+			}
+		}
+
+		engine.swap(next);
+	}
+
+	@Override
+	public void exit() {}
+}

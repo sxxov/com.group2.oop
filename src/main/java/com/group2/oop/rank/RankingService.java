4b014c0bf7389f55f1521dbf33c9d24feaa72d37
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-11-09 21:10:00 +0800

    Fix ranking service

--- src/main/java/com/group2/oop/rank/RankingService.java
+++ src/main/java/com/group2/oop/rank/RankingService.java
@@ -1,15 +1,17 @@
 package com.group2.oop.rank;
 
-import com.group2.oop.carbon_credits.CarbonCreditAccount;
+import com.group2.oop.account.AccountManager;
+import com.group2.oop.account.User;
+import com.group2.oop.account.UserRepository;
 import com.group2.oop.carbon_credits.CarbonCreditAccountRepository;
+import com.group2.oop.console.Console;
 import com.group2.oop.dependency.D;
 import com.group2.oop.home.HomeService;
 import com.group2.oop.service.Engine;
 import com.group2.oop.service.Service;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
-import java.util.UUID;
+import java.util.Scanner;
 
 public class RankingService implements Service {
 
@@ -27,26 +29,45 @@ public class RankingService implements Service {
 		CarbonCreditAccountRepository.class
 	);
 
-	private final CarbonCreditAccount carbonCreditAccount = D.get(
-		CarbonCreditAccount.class
-	);
+	private final UserRepository userRepository = D.get(UserRepository.class);
+	private final AccountManager account = D.get(AccountManager.class);
+	private final Scanner scanner = D.get(Scanner.class);
 
 	@Override
 	public void init(Engine engine) {
-		// Fetch the list of user IDs
-		List<UUID> userIds = getUsersWithCarbonCredits();
-
-		// Display the ranked users with their received carbon credits
-		for (int i = 0; i < userIds.size(); i++) {
-			UUID userId = userIds.get(i);
-			double carbonCredits = getCarbonCreditsForUser(userId);
+		main:for (;;) {
 			System.out.println(
-				(i + 1) +
-				". User " +
-				userId +
-				" - Received Carbon Credits: " +
-				carbonCredits
+				"------------------- Your ranking -------------------"
 			);
+			System.out.println("Global: #" + getCurrentUserRank());
+			System.out.println("");
+			System.out.println("1. View global leaderboard");
+			System.out.println("");
+			System.out.println("0. Exit");
+
+			int choice;
+			mainChoice:for (;;) {
+				System.out.print("> ");
+
+				try {
+					choice = Integer.parseInt(scanner.nextLine());
+				} catch (NumberFormatException e) {
+					System.out.println("Invalid choice.");
+					continue mainChoice;
+				}
+
+				switch (choice) {
+					case 1:
+						System.out.println("[View global leaderboard]\n");
+						printLeaderboard();
+
+						Console.waitForEnter();
+						continue main;
+					case 0:
+						System.out.println("[Exit]\n");
+						break main;
+				}
+			}
 		}
 
 		engine.swap(next);
@@ -55,13 +76,47 @@ public class RankingService implements Service {
 	@Override
 	public void exit() {}
 
-	private List<UUID> getUsersWithCarbonCredits() {
-		// Retrieve all user IDs with carbon credits from CarbonCreditAccountRepository
-		return new ArrayList<>(carbonCreditAccountRepository.getAllUserIds());
+	private int getCurrentUserRank() {
+		var userToAmount = getAllUserAndAmount();
+
+		var i = 0;
+		for (var entry : userToAmount) {
+			var user = entry.getKey();
+			if (user.uuid().equals(account.current().get().uuid())) {
+				return i;
+			}
+			++i;
+		}
+
+		return -1;
 	}
 
-	private double getCarbonCreditsForUser(UUID userId) {
-		// Use the CarbonCreditAccountRepository to retrieve the carbon credits for the user
-		return carbonCreditAccountRespository.email().balance();
+	private List<SimpleEntry<User, Double>> getAllUserAndAmount() {
+		return carbonCreditAccountRepository
+			.entrySet()
+			.stream()
+			.map(entry -> {
+				// get user & carbon credit amount first to be thread safe
+				var uuid = entry.getKey();
+				var user = userRepository.get(uuid);
+				var credit = entry.getValue();
+				var amount = credit.amount().get();
+
+				return new SimpleEntry<User, Double>(user, amount);
+			})
+			.sorted((a, b) -> a.getValue().compareTo(b.getValue()))
+			.toList();
+	}
+
+	private void printLeaderboard() {
+		var userToAmount = getAllUserAndAmount();
+
+		var i = 0;
+		for (var entry : userToAmount) {
+			var user = entry.getKey();
+			var amount = entry.getValue();
+			System.out.println((i + 1) + ". " + user.email() + "\t- " + amount);
+			++i;
+		}
 	}
 }
